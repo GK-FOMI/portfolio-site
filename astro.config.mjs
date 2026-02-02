ad73f93004af50dc4a3b18f1f81ef76f57df655e
Gabriel FOMI <gabrielofficiel237@gmail.com>
2026-02-02 02:57:38 +0100

    premier déploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import sanity from "@sanity/astro";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: "7eimp9kv",
+      dataset: "production",
+      useCdn: false,
+      studioUrl: "http://localhost:3333",
+    }),
+  ],
+});
---
+++ src/env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="astro/client" />
+/// <reference types="@sanity/astro/module" />
+
+declare module "sanity:client" {
+  export const sanityClient: import("@sanity/client").SanityClient;
+}
